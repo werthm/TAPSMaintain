7fd73a6a2070665ba34d6d887ac4290a459bd9d5
Dominik Werthmueller <werthmuellerd@gmail.com>
2017-02-18 00:28:16 +0100

    improved LED calibration

--- include/TMCalibLED.h
+++ include/TMCalibLED.h
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Author: Dominik Werthmueller, 2008-2013
+ * Author: Dominik Werthmueller, 2008-2017
  *************************************************************************/
 
 //////////////////////////////////////////////////////////////////////////
@@ -17,6 +17,7 @@
 #include "TMSeqCalibModule.h"
 
 class TGNumberEntry;
+class TLine;
 
 enum {
     kLED_Calib_Type_LG_LED1,
@@ -35,10 +36,12 @@ private:
     Char_t fCutHName[256];                      // name of the LED cut ADC spectra
     Double_t fXStart;                           // x-Axis start
     Double_t fXEnd;                             // x-Axis end
-    Double_t fLevel;                            // threshold level
+    Int_t fRebin;                               // rebin
     Double_t fRingVoltages[11];                 // threshold voltages set per ring
-    
+
+    Double_t fThreshold;                        // threshold
     TH1F* fHClone;                              // histogram pointer used for cloning
+    TLine* fLine;                               // marker line
 
     // Config dialog elements
     TGComboBox* fTypeCombo;                     // LED type selection combo box
@@ -47,7 +50,7 @@ private:
     TGHorizontalFrame* fAxisRangeFrame;         // frame for the x-Axis range GUI elements
     TGTextEntry* fXAxisStart;                   // start value of the x-Axis
     TGTextEntry* fXAxisEnd;                     // end value of the x-Axis
-    TGTextEntry* fThresholdLevel;               // threshold level input
+    TGTextEntry* fRebinEntry;                   // rebin input
     TGCheckButton* fBatchMode;                  // batch mode check box
     TGNumberEntry* fRing1Voltage;               // threshold voltage set for the ring 1
     TGNumberEntry* fRing2Voltage;               // threshold voltage set for the ring 2
@@ -60,21 +63,21 @@ private:
     TGNumberEntry* fRing9Voltage;               // threshold voltage set for the ring 9
     TGNumberEntry* fRing10Voltage;              // threshold voltage set for the ring 10
     TGNumberEntry* fRing11Voltage;              // threshold voltage set for the ring 11
-    
+
     Double_t FindThreshold(TH1F* h);
 
 public:
     TMCalibLED() : TMSeqCalibModule() { }
     TMCalibLED(const Char_t* name, UInt_t id);
     virtual ~TMCalibLED();
-    
+
     virtual void Init();
     virtual void ReadConfig();
     virtual void Process(Int_t index);
     virtual void Redo();
     virtual void Quit();
     virtual void SaveResults(const Char_t* filename);
-    
+
     void UpdateLEDType(Int_t id);
 
     ClassDef(TMCalibLED, 0) // LED calibration module
--- src/TMCalibLED.cxx
+++ src/TMCalibLED.cxx
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Author: Dominik Werthmueller, 2008-2013
+ * Author: Dominik Werthmueller, 2008-2017
  *************************************************************************/
 
 //////////////////////////////////////////////////////////////////////////
@@ -19,6 +19,7 @@
 #include "TGTextEntry.h"
 #include "TGNumberEntry.h"
 #include "TH1.h"
+#include "TF1.h"
 #include "TFile.h"
 #include "TText.h"
 #include "TLine.h"
@@ -33,21 +34,28 @@ TMCalibLED::TMCalibLED(const Char_t* name, UInt_t id)
     : TMSeqCalibModule(name, id, 1, kTRUE, kTRUE)
 {
     // Constructor.
-    
+
     // set module result header
     SetResultHeader("# Results of the LED Calibration module\n"
                     "# Format: element id, set LED threshold voltage, LED threshold channel\n");
-    
+
+    fThreshold = 0;
     fHClone = 0;
+
+    // marker line
+    fLine = new TLine();
+    fLine->SetLineStyle(1);
+    fLine->SetLineWidth(2);
+    fLine->SetLineColor(kRed);
 
     // create configuration dialog
     fConfigFrame->SetLayoutManager(new TGTableLayout(fConfigFrame, 17, 2));
-    
+
     // LED type
     TGLabel* l = new TGLabel(fConfigFrame, "LED Type:");
     l->SetTextJustify(kTextRight);
     fConfigFrame->AddFrame(l, new TGTableLayoutHints(0, 1, 0, 1, kLHintsFillX | kLHintsRight, 5, 5, 5, 5));
-    
+
     fTypeCombo = new TGComboBox(fConfigFrame);
     fTypeCombo->Connect("Selected(Int_t)", "TMCalibLED", this, "UpdateLEDType(Int_t)");
     fTypeCombo->Resize(200, 22);
@@ -57,24 +65,24 @@ TMCalibLED::TMCalibLED(const Char_t* name, UInt_t id)
     fTypeCombo->AddEntry("BaF2 LGS LED 2", kLED_Calib_Type_LGS_LED2);
     fTypeCombo->AddEntry("Manual", kLED_Calib_Type_MANUAL);
     fConfigFrame->AddFrame(fTypeCombo, new TGTableLayoutHints(1, 2, 0, 1, kLHintsFillX | kLHintsLeft, 5, 5, 2, 2));
-    
+
     // Uncut histogram name
     l = new TGLabel(fConfigFrame, "Uncut histogram name:");
     l->SetTextJustify(kTextRight);
     fConfigFrame->AddFrame(l, new TGTableLayoutHints(0, 1, 1, 2, kLHintsFillX | kLHintsRight, 5, 5, 5, 5));
-    
+
     fUncut = new TGTextEntry(fConfigFrame);
     fConfigFrame->AddFrame(fUncut, new TGTableLayoutHints(1, 2, 1, 2, kLHintsFillX | kLHintsRight, 5, 5, 2, 2));
-    
-    
+
+
     // LED cut histogram name
     l = new TGLabel(fConfigFrame, "LED cut histogram name:");
     l->SetTextJustify(kTextRight);
     fConfigFrame->AddFrame(l, new TGTableLayoutHints(0, 1, 2, 3, kLHintsFillX | kLHintsRight, 5, 5, 5, 5));
-    
+
     fCut = new TGTextEntry(fConfigFrame);
     fConfigFrame->AddFrame(fCut, new TGTableLayoutHints(1, 2, 2, 3, kLHintsFillX | kLHintsRight, 5, 5, 2, 2));
-    
+
     // threshold voltage number entries
     // ring 1
     l = new TGLabel(fConfigFrame, "Ring 1 Threshold [mV]:");
@@ -82,7 +90,7 @@ TMCalibLED::TMCalibLED(const Char_t* name, UInt_t id)
     fConfigFrame->AddFrame(l, new TGTableLayoutHints(0, 1, 3, 4, kLHintsFillX | kLHintsRight, 5, 5, 5, 5));
     fRing1Voltage = new TGNumberEntry(fConfigFrame, 1100, 6);
     fConfigFrame->AddFrame(fRing1Voltage, new TGTableLayoutHints(1, 2, 3, 4, kLHintsLeft, 5, 5, 2, 2));
-    
+
     // ring 2
     l = new TGLabel(fConfigFrame, "Ring 2 Threshold [mV]:");
     l->SetTextJustify(kTextRight);
@@ -153,12 +161,12 @@ TMCalibLED::TMCalibLED(const Char_t* name, UInt_t id)
     fRing11Voltage = new TGNumberEntry(fConfigFrame, 1100, 6);
     fConfigFrame->AddFrame(fRing11Voltage, new TGTableLayoutHints(1, 2, 13, 14, kLHintsLeft, 5, 5, 2, 2));
 
-    
+
     // x-Axis Range
     l = new TGLabel(fConfigFrame, "x-Axis Range:");
     l->SetTextJustify(kTextRight);
     fConfigFrame->AddFrame(l, new TGTableLayoutHints(0, 1, 14, 15, kLHintsFillX | kLHintsRight, 5, 5, 5, 5));
-    
+
     fAxisRangeFrame = new TGHorizontalFrame(fConfigFrame);
     fXAxisStart = new TGTextEntry(fAxisRangeFrame, "0");
     fXAxisEnd = new TGTextEntry(fAxisRangeFrame, "300");
@@ -168,25 +176,25 @@ TMCalibLED::TMCalibLED(const Char_t* name, UInt_t id)
     fAxisRangeFrame->AddFrame(new TGLabel(fAxisRangeFrame, "to"), new TGLayoutHints(kLHintsExpandY, 5, 5, 2, 2));
     fAxisRangeFrame->AddFrame(fXAxisEnd, new TGLayoutHints(kLHintsExpandY, 5, 5, 2, 2));
     fConfigFrame->AddFrame(fAxisRangeFrame, new TGTableLayoutHints(1, 2, 14, 15, kLHintsLeft, 0, 0, 0, 0));
-    
-    
-    // Threshold level
-    l = new TGLabel(fConfigFrame, "Threshold Level:");
-    l->SetTextJustify(kTextRight); 
+
+
+    // Rebin
+    l = new TGLabel(fConfigFrame, "Rebin:");
+    l->SetTextJustify(kTextRight);
     fConfigFrame->AddFrame(l, new TGTableLayoutHints(0, 1, 15, 16, kLHintsFillX | kLHintsRight, 5, 5, 5, 5));
-    
-    fThresholdLevel = new TGTextEntry(fConfigFrame, "0.9");
-    fThresholdLevel->Resize(40, 22);
-    fConfigFrame->AddFrame(fThresholdLevel, new TGTableLayoutHints(1, 2, 15, 16, kLHintsLeft, 5, 5, 2, 2));
-    
-    
+
+    fRebinEntry = new TGTextEntry(fConfigFrame, "2");
+    fRebinEntry->Resize(40, 22);
+    fConfigFrame->AddFrame(fRebinEntry, new TGTableLayoutHints(1, 2, 15, 16, kLHintsLeft, 5, 5, 2, 2));
+
+
     // Batch mode
     fBatchMode = new TGCheckButton(fConfigFrame, "Batch mode");
     fConfigFrame->AddFrame(fBatchMode, new TGTableLayoutHints(1, 2, 16, 17, kLHintsFillX | kLHintsRight, 5, 5, 5, 5));
-    
+
     // needs to be called in every TMModule that uses a configuration dialog
     fConfigFrame->Layout();
-    
+
     // select LED type
     fTypeCombo->Select(kLED_Calib_Type_LG_LED1, kTRUE);
 }
@@ -197,16 +205,17 @@ TMCalibLED::~TMCalibLED()
     // Destructor.
 
     if (fHClone) delete fHClone;
+    if (fLine) delete fLine;
 }
 
 //______________________________________________________________________________
 void TMCalibLED::Init()
 {
     // (Re-)initalize the module.
-    
+
     // Clear results array
     ClearResults();
-    
+
     // check if batch mode was selected
     if (fBatchMode->IsOn())
     {
@@ -222,34 +231,30 @@ void TMCalibLED::Init()
 //______________________________________________________________________________
 void TMCalibLED::Redo()
 {
-    // Redo the current elements but change x-Axis range to 
-    // look for the threshold limit in.
+    // Read the threshold value from the line.
 
-    // backup old range values
-    Double_t oldStart = fXStart;
-    Double_t oldEnd = fXEnd;
-
-    // get user selected range
-    fXStart = fHClone->GetXaxis()->GetBinCenter(fHClone->GetXaxis()->GetFirst());
-    fXEnd = fHClone->GetXaxis()->GetBinCenter(fHClone->GetXaxis()->GetLast());
-
-    ProcessElement(fCurrentElement);
-
-    // reset old range values
-    fXStart = oldStart;
-    fXEnd = oldEnd;
+    SetResult(fCurrentElement, 0, fLine->GetX1());
 }
 
 //______________________________________________________________________________
 void TMCalibLED::Process(Int_t index)
 {
     // Fit LED spectrum of the element 'index'.
-    
+
     Char_t name[256];
     TH1F* h1;
     TH1F* h2;
-    Double_t threshold;
-    
+
+    // check if previous element was corrected
+    if (index)
+    {
+        if (fLine->GetX1() != fThreshold)
+        {
+            SetResult(index-1, 0, fLine->GetX1());
+            Info("Process", "Corrected threshold of element %d to %f", index-1+1, fLine->GetX1());
+        }
+    }
+
     // load raw spectrum
     sprintf(name, fUncutHName, index + 1);
     h1 = (TH1F*) fFile->Get(name);
@@ -257,7 +262,7 @@ void TMCalibLED::Process(Int_t index)
     // load LED spectrum
     sprintf(name, fCutHName, index + 1);
     h2 = (TH1F*) fFile->Get(name);
-    
+
     // check if spectra could be loaded
     if (!h1)
     {
@@ -266,7 +271,7 @@ void TMCalibLED::Process(Int_t index)
         Finished();
         return;
     }
-    
+
     if (!h2)
     {
         sprintf(name, "Spectrum '%s' was not found in ROOT file!", fCutHName);
@@ -274,47 +279,56 @@ void TMCalibLED::Process(Int_t index)
         Finished();
         return;
     }
-    
+
     // clone spectrum
     if (fHClone) delete fHClone;
     fHClone = (TH1F*) h2->Clone();
-    
+
     // divide LED spectrum
     fHClone->Divide(h1);
-    fHClone->Rebin(2);
-    fHClone->Scale(1./2.);
+    fHClone->Rebin(fRebin);
+    fHClone->Scale(1./(Double_t)fRebin);
+
+    // prepare histogram
+    for (Int_t i = 1; i <= fHClone->GetNbinsX(); i++)
+    {
+        if (fHClone->GetBinCenter(i) < fXStart ||
+            fHClone->GetBinCenter(i) > fXEnd)
+        {
+            fHClone->SetBinContent(i, 0);
+            fHClone->SetBinError(i, 0);
+        }
+    }
 
     // find LED threshold position
-    threshold = FindThreshold(fHClone);
-    
+    fThreshold = FindThreshold(fHClone);
+
+    // correct bad values
+    if (fThreshold != 0 && (fThreshold < fXStart || fThreshold > fXEnd))
+        fThreshold = (fXStart + fXEnd) / 2.;
+
     // save result
-    SetResult(index, 0, threshold);
-    
+    SetResult(index, 0, fThreshold);
+
     // draw spectrum
-    fHClone->Draw();
+    fHClone->Draw("hist");
     fHClone->GetXaxis()->SetRangeUser(fXStart, fXEnd);
     fHClone->GetYaxis()->SetRangeUser(0, 1.1);
     fHClone->GetXaxis()->SetTitle("ADC Channel");
     fHClone->GetYaxis()->SetTitle("LED divided by raw");
-    
+
     // draw text
     TText aText;
     aText.SetNDC(kTRUE);
-    sprintf(name, "Thr.: %5.2f", threshold);
+    sprintf(name, "Thr.: %5.2f", fThreshold);
     aText.DrawText(0.1, 0.91, name);
 
-    // draw level line
-    TLine aLine;
-    aLine.SetLineStyle(2);
-    aLine.SetLineWidth(1);
-    aLine.SetLineColor(kBlack);
-    aLine.DrawLine(fXStart, fLevel, fXEnd, fLevel);
-
     // mark threshold position
-    aLine.SetLineStyle(1);
-    aLine.SetLineWidth(2);
-    aLine.SetLineColor(kRed);
-    aLine.DrawLine(threshold, 0, threshold, 1);
+    fLine->SetX1(fThreshold);
+    fLine->SetX2(fThreshold);
+    fLine->SetY1(0);
+    fLine->SetY2(1);
+    fLine->Draw();
 }
 
 //______________________________________________________________________________
@@ -322,42 +336,42 @@ void TMCalibLED::SaveResults(const Char_t* filename)
 {
     // Save the results array to the file 'filename'. Use numberFormat to format the array
     // content. Overwritten method of class TMModule to customize number output format
-    
+
     // open output file
     FILE* fout;
     fout = fopen(filename, "w");
-    
+
     // save module result header
     fprintf(fout, "%s", GetResultHeader());
     fprintf(fout, "\n");
-    
+
     Double_t** results = GetResults();
-    
+
     // print array content
     for (Int_t i = 0; i < GetCurrentDetectorSize(); i++)
     {
         // get element threshold voltage
         Int_t thresVol = (Int_t)fRingVoltages[TMUtils::GetRingNumber(i)-1];
-        
-        fprintf(fout, "%3d  %5d  %7.3f\n", i+1, thresVol, results[i][0]); 
+
+        fprintf(fout, "%3d  %5d  %7.3f\n", i+1, thresVol, results[i][0]);
     }
-    
+
     fclose(fout);
-    
+
     Info("SaveResults", "Saved results of module '%s' to file '%s'", GetName(), filename);
 }
- 
+
 //______________________________________________________________________________
 void TMCalibLED::Quit()
 {
     // Save LED calibration results and quit module.
-    
+
     // dump results
     //DumpResults("%6.2f");
-    
+
     // save results
     Save();
-        
+
     // emit Finished() signal
     Finished();
 }
@@ -365,27 +379,29 @@ void TMCalibLED::Quit()
 //______________________________________________________________________________
 Double_t TMCalibLED::FindThreshold(TH1F* h)
 {
-    // Find the position where the bin content of h changes from
-    // < fLevel to > fLevel.
+    // Find the threshold position.
 
-    Double_t pos;
-    Double_t posUnder = 0;
-    Double_t content;
+    // check histo
+    if (h->Integral() < 10) return 0;
 
-    // loop over all bins
-    for (Int_t i = 0; i < h->GetNbinsX(); i++)
-    {
-        pos = h->GetBinCenter(i);
-        content = h->GetBinContent(i);
-     
-        if (pos < fXStart || pos > fXEnd) continue;
+    // derive histogram
+    TH1* deriv = TMUtils::DeriveHistogram(h);
+    TMUtils::ZeroBins(deriv);
 
-        if (content > fLevel) return (pos + posUnder) / 2.;   
-        else posUnder = pos;
-    }
-    
-    // return 0 if position was not found
-    return 0;
+    // get maximum value
+    Double_t pos = deriv->GetBinCenter(deriv->GetMaximumBin());
+
+    // fit
+    TF1* func = new TF1("func", "gaus", pos-20, pos+20);
+    func->SetParameters(1, pos, 1);
+    deriv->Fit(func, "RQ0");
+    pos = func->GetParameter(1);
+
+    // clean-up
+    delete deriv;
+    delete func;
+
+    return pos;
 }
 
 //______________________________________________________________________________
@@ -393,7 +409,7 @@ void TMCalibLED::UpdateLEDType(Int_t id)
 {
     // Update the settings in the configuration dialog for the LED type the
     // user chose in the combo box
-    
+
     if (id == kLED_Calib_Type_LG_LED1)
     {
         fUncut->SetText("BaF2_LG_%03d");
@@ -435,7 +451,7 @@ void TMCalibLED::UpdateLEDType(Int_t id)
 void TMCalibLED::ReadConfig()
 {
     // Read the configuration made by the user in the config dialog.
-    
+
     // set detector id
     Int_t type = fTypeCombo->GetSelected();
     if (type == kLED_Calib_Type_LG_LED1)         fDetID = kBaF2_Detector;
@@ -443,18 +459,18 @@ void TMCalibLED::ReadConfig()
     else if (type == kLED_Calib_Type_LGS_LED1)   fDetID = kBaF2_Detector;
     else if (type == kLED_Calib_Type_LGS_LED2)   fDetID = kBaF2_Detector;
     else if (type == kLED_Calib_Type_MANUAL)     fDetID = kBaF2_Detector;
-    
+
     // copy histogram names
     strcpy(fUncutHName, fUncut->GetText());
     strcpy(fCutHName, fCut->GetText());
-    
+
     // copy x-Axis range values
     fXStart = atof(fXAxisStart->GetText());
     fXEnd = atof(fXAxisEnd->GetText());
-    
-    // copy threshold level
-    fLevel = atof(fThresholdLevel->GetText());
-    
+
+    // copy rebin
+    fRebin = atoi(fRebinEntry->GetText());
+
     // read ring threshold voltages
     fRingVoltages[0] = fRing1Voltage->GetNumber();
     fRingVoltages[1] = fRing2Voltage->GetNumber();
